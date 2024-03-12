43c718be99f8c8e1eefd8a417d0a497f2035e79e
Abdul Khok <khok.abdul@gmail.com>
2024-03-12 21:28:15 +0200

    added recieve and send events for client and server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const formatMessage = require("./utils/formatMessage.js");
+
+const {
+    addPlayer,
+    getAllPlayers,
+    getPlayer,
+    removePlayer,
+} = require("./utils/players.js");
 
 const port = process.env.PORT || 8080;
 
@@ -12,8 +20,19 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => { // listen for new connections to Socket.IO
+io.on('connection', socket => {
     console.log('A new player just connected');
+
+    socket.on('join', ({ playerName, room }, callback) => {
+        const { error, newPlayer } = addPlayer({ id: socket.id, playerName, room });
+
+        if (error) return callback(error.message);
+        callback();
+
+        socket.join(newPlayer.room);
+
+        socket.emit('message', formatMessage('Admin', 'Welcome!'));
+    });
 })
 
 server.listen(port, () => {
